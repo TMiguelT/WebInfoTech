9b9ae2bf2b3ea243118a4364b88147f049122574
Jake Moxey <jakemoxey@gmail.com>
2015-03-31 22:34:40 +1100

    removed google maps import from index.html as angular already does it :)

--- app/game/gameController.js
+++ app/game/gameController.js
@@ -4,7 +4,7 @@
     function gameController($scope, $routeParams) {
         $scope.photoId = $routeParams.photoId;
 
-        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+        $scope.map = { center: { latitude: 40, longitude: -73 }, zoom: 8 };
     }
 
     angular
