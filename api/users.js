5aebbbe690f2bd9d11d968c7e97f334d0ca30902
TMiguelT <michael.r.milton@gmail.com>
2015-04-04 16:15:01 +1100

    Added user registration

--- api/users.js
+++ api/users.js
@@ -1,9 +1,48 @@
 var router = require('koa-router')();
+var bcrypt = require('bcrypt-as-promised');
 
 router
     //These are two different ways of performing the same query
     .post('/register', function *() {
-        this.body = yield this.knex('tag').where('tag_id', '>', 1);
+
+        var form = this.request.body;
+
+        //Validate parameters
+        this.checkBody('username').notEmpty().len(5, 20, "Username must be between 5 and 20 characters.");
+        this.checkBody('email').notEmpty().isEmail("Invalid email entered.");
+        this.checkBody('password').notEmpty().len(3, 20, "Password must be between 3 and 20 characters.");
+        this.checkBody('confirmPassword').eq(form.password, "Both the password fields must be the same.");
+
+        //If one of the parameters is invalid, throw an error
+        if (this.errors) {
+            this.status = 400;
+            this.body = this.errors;
+            return;
+        }
+
+        //Encrypt the password
+        var pw = yield bcrypt.hash(form.password, 10);
+
+        //Try to insert the new row
+        try {
+            yield this
+                .knex('user')
+                .insert({
+                    username: form.username,
+                    email: form.email,
+                    password: pw
+                });
+        }
+
+            //If there's an error (e.g. an existing username or email) throw an error
+        catch (e) {
+            this.status = 400;
+            this.body = e.message;
+            return;
+        }
+
+        //Otherwise the registration succeeded
+        this.body = "Success!";
     });
 
 module.exports = router.routes();
--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 var app = require('koa')();
-var static = require('koa-static');
 var mount = require('koa-mount');
-var router = require('koa-router');
 var send = require('koa-send');
-var logger = require('koa-logger');
 var path = require('path');
-var knex = require('koa-knex');
 
 var indexFile = path.join(__dirname, 'app/index.html');
 
 app
-    .use(knex({
+    .use(require('koa-knex')({
         client: 'pg',
         connection: {
             host     : '192.241.210.241',
@@ -19,12 +15,13 @@ app
             database : 'webinfotech'
         }
     })) //Use the database connection
-    .use(logger()) //Log each request
-    .use(router(app)) //Use the router top level router
+    .use(require('koa-logger')()) //Log each request
+    .use(require('koa-router')(app)) //Use the router top level router
+    .use(require('koa-body')()) //Use the form parser
+    .use(require('koa-validate')()) //Mount the form validator
     .use(mount("/api", require("./api/users"))) //Mount the users API
     .use(mount("/api", require("./api/photos"))) //Mount the users API
- // .use(mount("/api", require("./api/about"))) //Mount the about page API
-    .use(mount("/public", static("public"))) //Mount the static file server
+    .use(mount("/public", require('koa-static')("public"))) //Mount the static file server
     .use(function *() { //When the user goes anywhere that's not the api or public, send them the main page
         yield send(this, indexFile);
     });
