f4cdf900e279d05d1ebaef59d6d2ac2ee7a6ed3c
Jake Moxey <jakemoxey@gmail.com>
2015-04-08 20:54:36 +1000

    Removed animated nav manu effect on scroll

--- app/about/aboutController.js
+++ app/about/aboutController.js
@@ -5,17 +5,6 @@
         $scope.goTo = function(elName) {
             scrollService.goTo(elName)
         }
-
-        $(window).scroll(function() {
-            var scrollVal = $(window).scrollTop();
-
-            $(".overlay").css("opacity", scrollVal / 400);
-
-            if (scrollVal >= $('.main').offset().top)
-                $(".navbar").addClass('fadeInDown animated');
-            else
-                $(".navbar").removeClass('fadeInDown animated');
-        });
     }
 
     angular
