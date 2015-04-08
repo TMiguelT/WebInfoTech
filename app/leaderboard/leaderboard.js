99c7c6edc5b436b7523578c0c984ddaf2b65ab8a
Emma Jamieson <ejamieson19@gmail.com>
2015-04-08 12:03:35 +1000

    Added data to leaderboard html file and javascript file

---
+++ app/leaderboard/leaderboard.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Emma Jamieson-Hoare
+ */
+
+
+
+angular.module('app')
+    .controller('leaderboardController', function () {
+
+        this.players = PlayerList;
+
+    }
+
+
+
+
+PlayerList = [
+
+    {
+        Rank: "1",
+        Username: "emma0987",
+        Score: "5"
+    },
+
+    {
+        Rank: "2",
+        Username: "jakemoxey",
+        Score: "3"
+    },
+
+    {
+        Rank: "3",
+        Username: "brandonlyly",
+        Score: "0"
+    }
+
+]
