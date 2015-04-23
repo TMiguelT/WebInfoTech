1c8f1d7b6e09c72261a07e33b1e3a963f96fa176
Jake Moxey <jakemoxey@gmail.com>
2015-04-24 01:08:09 +1000

    Added function to select photo by ID

--- api/photo/photo_query.js
+++ api/photo/photo_query.js
@@ -1,5 +1,8 @@
 module.exports = {
     selectAllPhotos: function *(photos, knex) {
         return (yield knex.raw('SELECT image_path, name, description, date_created, num_finds, ST_X(ST_AsText("location")) AS location_lon, ST_Y(ST_AsText("location")) AS location_lat, ST_X(ST_AsText("orientation")) AS orientation_alpha, ST_Y(ST_AsText("orientation")) AS orientation_beta, user_id, photo_id FROM "photo"')).rows;
+    },
+    selectPhotoById: function *(photo_id, knex) {
+    return (yield knex.raw('SELECT image_path, name, description, date_created, num_finds, ST_X(ST_AsText("location")) AS location_lon, ST_Y(ST_AsText("location")) AS location_lat, ST_X(ST_AsText("orientation")) AS orientation_alpha, ST_Y(ST_AsText("orientation")) AS orientation_beta, user_id, photo_id FROM "photo" WHERE photo_id = ?', [photo_id])).rows[0];
     }
 }
