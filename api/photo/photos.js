c2c39a54c8551d0ebc93cf90c50431cbd49b7284
Jake Moxey <jakemoxey@gmail.com>
2015-04-24 01:09:49 +1000

    Photo Service now retrieves from DB yay

--- api/photo/photos.js
+++ api/photo/photos.js
@@ -1,15 +1,16 @@
 var photoMapper = require('./photo_mapper');
 var photoQuery = require('./photo_query');
 var router = require('koa-router')();
+var co = require('co');
 var photoData = require("./photoDummyData.json");
 
 router
-    .get('/all', function *(next) {
+    .get('/all_mock', function *(next) {
         this.body = {
             photos: photoData
         };
     })
-    .get('/all_test', function *() {
+    .get('/all', function *() {
         try {
             body_json = {photos: []};
 
@@ -39,6 +40,41 @@ router
             };
         } catch(e) {
             body_json = { error: String(e) };
+            console.error(e);
+        }
+
+        this.body = body_json;
+    })
+    .get('/:photoId', function *() {
+        try {
+            body_json = {};
+            photo_id = this.params.photoId;
+
+            var photo = yield photoQuery.selectPhotoById(photo_id, this.knex);
+
+            photo_json = {}
+
+            photoMapper.mapId(photo, photo_json);
+            photoMapper.mapUrl(photo, photo_json);
+            photoMapper.mapName(photo, photo_json);
+            photoMapper.mapDescription(photo, photo_json);
+            photoMapper.mapDatePosted(photo, photo_json);
+            photoMapper.mapFinds(photo, photo_json);
+            photoMapper.mapLocation(photo, photo_json);
+            photoMapper.mapOrientation(photo, photo_json);
+
+            yield photoMapper.mapUser(photo, photo_json, this.knex);
+
+            yield photoMapper.mapTags(photo, photo_json, this.knex);
+
+            yield photoMapper.mapComments(photo, photo_json, this.knex);
+
+            yield photoMapper.mapLikes(photo, photo_json, this.knex);
+
+            body_json = photo_json;
+        } catch(e) {
+            body_json = { error: String(e) };
+            console.error(e);
         }
 
         this.body = body_json;
