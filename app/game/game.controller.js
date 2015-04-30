c738b4bdf301a204d640a3903c9a86d6206f662b
Jake Moxey <jakemoxey@gmail.com>
2015-04-30 19:50:58 +1000

    now gets location when a photo is taken

--- app/game/game.controller.js
+++ app/game/game.controller.js
@@ -70,37 +70,46 @@
         }
 
         function setFoundPhotoButton() {
+            $scope.photoLocation = {
+                orientation: {},
+                coords: {}
+            }
             $scope.isOrientationCapable = false;
 
             if (window.DeviceOrientationEvent) {
                 window.addEventListener('deviceorientation', function(orientation){
                     if(orientation.alpha) {
                         $scope.isOrientationCapable = true;
-
-                        $(document).on('change', '.btn-file :file', function () {
-                            var input = $(this),
-                                numFiles = input.get(0).files ? input.get(0).files.length : 1,
-                                label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-                            input.trigger('fileselect', [numFiles, label]);
-                        });
-
-                        $(document).ready(function () {
-                            $('.btn-file :file').on('fileselect', function (event, numFiles, label) {
-
-                                var input = $(this).parents('.input-group').find(':text'),
-                                    log = numFiles > 1 ? numFiles + ' files selected' : label;
-
-                                if (input.length) {
-                                    input.val(log);
-                                } else {
-                                    if (log) alert(log);
-                                }
-
-                            });
-                        });
+                        $scope.photoLocation.orientation = orientation;
                     }
                 });
             }
+
+            $(document).on('change', '.btn-file :file', function () {
+                var input = $(this),
+                    numFiles = input.get(0).files ? input.get(0).files.length : 1,
+                    label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+                input.trigger('fileselect', [numFiles, label]);
+            });
+
+            $(document).ready(function () {
+                $('.btn-file :file').on('fileselect', function (event, numFiles, label) {
+
+                    var input = $(this).parents('.input-group').find(':text'),
+                        log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+                    if (input.length) {
+                        input.val(log);
+                    } else {
+                        if (log) {
+                            navigator.geolocation.getCurrentPosition(function(position) {
+                                $scope.photoLocation.coords = position.coords;
+                            });
+                        };
+                    }
+
+                });
+            });
         }
 
         function setHeight() {
