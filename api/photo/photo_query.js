cc63669b30fc90b7b3e70f68df5a003a71847e9d
Jake Moxey <jakemoxey@gmail.com>
2015-05-03 22:19:43 +1000

    fixed queries for new photo schema

--- api/photo/photo_query.js
+++ api/photo/photo_query.js
@@ -2,10 +2,10 @@ var moment = require('moment');
 
 module.exports = {
     selectAllPhotos: function *(photos, knex) {
-        return (yield knex.raw('SELECT image_path, name, description, date_created, num_finds, ST_X(ST_AsText("location")) AS location_lon, ST_Y(ST_AsText("location")) AS location_lat, ST_X(ST_AsText("orientation")) AS orientation_alpha, ST_Y(ST_AsText("orientation")) AS orientation_beta, user_id, photo_id FROM "photo"')).rows;
+        return (yield knex.raw('SELECT image_path, name, description, date_created, num_finds, ST_X(ST_AsText("location")) AS location_lon, ST_Y(ST_AsText("location")) AS location_lat, orientation[0] AS orientation_absolute, orientation[1] AS orientation_alpha, orientation[2] AS orientation_beta, orientation[3] AS orientation_gamma, user_id, photo_id FROM "photo"')).rows;
     },
     selectPhotoById: function *(photo_id, knex) {
-        return (yield knex.raw('SELECT image_path, name, description, date_created, num_finds, ST_X(ST_AsText("location")) AS location_lon, ST_Y(ST_AsText("location")) AS location_lat, ST_X(ST_AsText("orientation")) AS orientation_alpha, ST_Y(ST_AsText("orientation")) AS orientation_beta, user_id, photo_id FROM "photo" WHERE photo_id = ?', [photo_id])).rows[0];
+        return (yield knex.raw('SELECT image_path, name, description, date_created, num_finds, ST_X(ST_AsText("location")) AS location_lon, ST_Y(ST_AsText("location")) AS location_lat, orientation[0] AS orientation_absolute, orientation[1] AS orientation_alpha, orientation[2] AS orientation_beta, orientation[3] AS orientation_gamma, user_id, photo_id FROM "photo" WHERE photo_id = ?', [photo_id])).rows[0];
     },
     addComment: function *(comment, knex) {
         return (yield knex("comment")
