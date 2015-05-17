71e2dffe7d6ec7e119bd46140a8c5dea340491d7
Jake Moxey <jakemoxey@gmail.com>
2015-05-18 01:22:08 +1000

    fixed query

--- api/leaderboard/sql_queries/user_select_query.sql
+++ api/leaderboard/sql_queries/user_select_query.sql
@@ -1,7 +1,6 @@
 SELECT json_build_object(
         'user_id', "user".user_id,
         'username', "user".username,
-        'user_id', "user".user_id,
         'score', 5*COUNT("like".VALUE) + 10*COUNT("find")
 ) AS "user"
 FROM "user"
