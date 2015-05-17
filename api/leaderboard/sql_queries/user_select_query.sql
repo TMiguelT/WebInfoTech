f49a743e9e870b0b80e089277dbf5f9d397a36f9
Emma Jamieson <ejamieson19@gmail.com>
2015-05-18 00:24:33 +1000

    sorted by user_id in query

--- api/leaderboard/sql_queries/user_select_query.sql
+++ api/leaderboard/sql_queries/user_select_query.sql
@@ -1,8 +1,9 @@
 SELECT json_build_object(
         'username', "user".username,
+        'user_id', "user".user_id,
         'score', 5*COUNT("like".VALUE) + 10*COUNT("find")
 ) AS "user"
 FROM "user"
 LEFT OUTER JOIN "like" ON "user".user_id="like".user_id
 LEFT OUTER JOIN "find" ON "user".user_id="find".user_id
-GROUP BY "user".username
+GROUP BY "user".user_id
